4bfb9e0832714b4c1b31e62b2961e18b558897de
Liu Hao <18766382928@163.com>
2024-12-03 16:48:38 +0800

    12.3

--- ptr11/ptr/yuanshi_zimianliang.cpp
+++ ptr11/ptr/yuanshi_zimianliang.cpp
@@ -2,12 +2,26 @@
 using namespace std;
 int main()
 {
-    string str = "D:\hello\world\test.text";
-    cout << str << endl;
+    // string str = "D:\hello\world\test.text";
+    // cout << str << endl;
+    //vscode直接报错
     string str1 = "D:\\hello\\world\\test.text";
     cout << str1 << endl;
     string str2 = R"(D:\hello\world\test.text)";
     cout << str2 << endl;
+        string str = R"(<html>
+        <head>
+        <title>
+        海贼王
+        </title>
+        </head>
+        <body>
+        <p>
+        我是要成为海贼王的男人!!!
+        </p>
+        </body>
+        </html>)";
+    cout << str << endl;
 
-
+    return 0;
 }
